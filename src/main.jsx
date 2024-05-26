3ad9fa7180ee071ef07ef270c64ccb9cfc6c416e
Marcos-Castillo <93360137+Marcos-Castillo@users.noreply.github.com>
2024-05-26 20:10:26 -0300

    Se agrega ruteo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={
+
+                            <App />
+
+                    } />
+                    <Route path="/product/:idProduct" element={<Header/>} />
+                </Routes>
+            </BrowserRouter>
 )
